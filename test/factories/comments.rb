4d4dd02bd7f7264903eed36a09b9348522db213b
Bonnie Simon <bonnie.developer@gmail.com>
2022-08-31 19:42:51 +0530

    Added comments to tasks

---
+++ test/factories/comments.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# FactoryBot.define do
+#   factory :comment do
+#     content { "MyText" }
+#     task { nil }
+#     user { nil }
+#   end
+# end
+
+# I don't know how this file was generated.
+# It got generated in chapter 52.
+# I was getting an error because of this file. Because we were to create a file called comment.rb with somewhat similar content.
