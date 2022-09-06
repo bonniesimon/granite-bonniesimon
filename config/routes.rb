1eb81bee2e4f9617a3e6ed29c20aa800e1d56220
Bonnie Simon <bonnie.developer@gmail.com>
2022-09-06 12:45:29 +0530

    Add the sidekiq dashboard to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
+require "sidekiq/web"
+
+require "sidekiq/cron/web"
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  def draw(routes_name)
+    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
+  end
+
+  draw :sidekiq
+
   constraints(lambda { |req| req.format == :json }) do
-    resources :tasks, except: %i[new edit], param: :slug
+    resources :tasks, except: %i[new edit], param: :slug do
+      collection do
+        resource :report, only: %i[create], module: :tasks do
+          get :download, on: :collection
+        end
+      end
+    end
     resources :users, only: %i[index create]
     resource :session, only: [:create, :destroy]
     resources :comments, only: :create
